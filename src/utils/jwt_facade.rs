65933abb5bbb7caa8fea52d0ebee65de086e051a
AlexRozaLopes <alexrozaestudos@gmail.com>
2025-03-07 19:26:50 -0300

    git actions

--- src/utils/jwt_facade.rs
+++ src/utils/jwt_facade.rs
@@ -4,9 +4,11 @@ use jwt::SignWithKey;
 use serde::{Deserialize, Serialize};
 use sha2::Sha256;
 use std::collections::BTreeMap;
+use std::env;
 
 pub fn create_jwt_for_user(user: User) -> JwtToken {
-    let key: Hmac<Sha256> = Hmac::new_from_slice(b"dontpanic42").unwrap();
+    let secret = env::var("JWT_SECRET").expect("JWT_SECRET must be set");
+    let key: Hmac<Sha256> = Hmac::new_from_slice(secret.as_ref()).unwrap();
     let mut claims = BTreeMap::new();
     claims.insert("user_id", user.id.to_string());
     claims.insert("email", user.email.to_string());
